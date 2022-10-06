dcc00f29852c4ec66b613cc71e7230ef6c05c15c
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-06 13:01:57 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public form: FormGroup;
+  public form = this.fb.group({
+    name: '',
+    code: '',
+    address: [
+      {
+        address:
+          '78 Rue du Moulin Vert, Paris 14e Arrondissement, Île-de-France, France',
+        lat: 48.8304,
+        lng: 2.32209,
+      },
+    ],
+  });
 
   constructor(public fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.form = this.fb.group({
-      name: [''],
-      code: [''],
-    });
-  }
 }
--- src/app/input-address/input-address.component.ts
+++ src/app/input-address/input-address.component.ts
@@ -1,39 +1,83 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import * as mapboxgl from 'mapbox-gl';
-import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 @Component({
   selector: 'app-input-address',
   templateUrl: './input-address.component.html',
   styleUrls: ['./input-address.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => InputAddressComponent),
+    },
+  ],
 })
-export class InputAddressComponent implements AfterViewInit {
+export class InputAddressComponent
+  implements AfterViewInit, ControlValueAccessor
+{
   private accessToken =
     'pk.eyJ1IjoiZHltYWZyIiwiYSI6ImNsODNjMmp4MTAwY2UzcHA4ZWhmaG5sNDAifQ.ozVmzxmotxaZbXfnvHKYDw';
   private map!: mapboxgl.Map;
+  private marker!: mapboxgl.Marker;
   private autocomplete!: MapboxGeocoder;
+  public innerValue!: {
+    address: string;
+    lat: number;
+    lng: number;
+  };
+  private onChange: any;
+  private onTouched: any;
 
   constructor() {}
 
-  ngAfterViewInit() {
-    this.map = new mapboxgl.Map({
-      accessToken: this.accessToken,
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      zoom: 9,
-      center: [-74.5, 40],
-    });
+  writeValue(value: any) {
+    this.innerValue = value;
+  }
 
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(disabled: boolean) {}
+
+  ngAfterViewInit() {
     this.autocomplete = new MapboxGeocoder({
       accessToken: this.accessToken,
       mapboxgl: mapboxgl,
+      placeholder: this.innerValue.address,
     });
 
     this.autocomplete.on('result', (e: any) => {
       if (e.result) {
-        console.log(e);
+        if (this.marker) {
+          this.marker.remove();
+        }
+        this.innerValue.lat = e.result.center[1];
+        this.innerValue.lng = e.result.center[0];
+        this.innerValue.address = e.result.place_name;
+        this.onChange(this.innerValue);
       }
     });
+
+    this.map = new mapboxgl.Map({
+      accessToken: this.accessToken,
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 13,
+      center: [this.innerValue.lng, this.innerValue.lat],
+    });
+
+    if (this.innerValue.lng) {
+      this.marker = new mapboxgl.Marker()
+        .setLngLat([this.innerValue.lng, this.innerValue.lat])
+        .addTo(this.map);
+    }
 
     this.map.addControl(this.autocomplete);
   }
